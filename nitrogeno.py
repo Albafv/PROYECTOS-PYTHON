1c2c30f5468b8d45c58cc50a1b70d13f8f36c54d
Albafv <fernandezvazquezalba16@gmail.com>
2024-12-11 13:06:47 +0100

    nitrogeno

--- nitrogeno.py
+++ nitrogeno.py
@@ -1,8 +1,11 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 import random
 # Lista random de temperaturas entre -500 y 500 º
 temperaturas = [random.randint(-700, 700) for _ in range(100)]
 #Filtrado de temperaturas mayores que -196 ºC
-gaseoso =  [temp for temp in temperaturas if (temp > -196)]
-# Imprimimo las temperaturas y cuántas cumplen la condición
+crit=-196
+gaseoso =  [temp for temp in temperaturas if (temp >= crit)]
+# Imprimimos las temperaturas y cuántas cumplen la condición
 print("Temperaturas donde el nitrógeno es gaseoso:", gaseoso)
 print("En ", len(gaseoso) ," ocasiones, el nitrógeno se encontraba en estado gas")
